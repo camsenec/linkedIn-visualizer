35509d97375b097fe5483912ff425928b99f1423
camsenec <deepsky2221@gmail.com>
2021-04-27 15:34:03 +0300

    app: input form done

--- app/app.py
+++ app/app.py
@@ -4,19 +4,17 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go # or plotly.express as px
 import datetime
-import graphs
+from dash.dependencies import Input, Output, State
+import visualizer
 
 app = dash.Dash(__name__)
 app.title = "LinkedIn Visualizer"
 
 connections_df = pd.read_csv('../input/sample1/Connections.csv')
 connections_df["Connected On"] = connections_df["Connected On"].apply(lambda x: datetime.datetime.strptime(x, "%d-%b-%y").strftime("%Y-%m-%d"))
-connections_df["Company"]
 
-app.layout = html.Div(
-    children=[
-        
-      html.Div(
+app.layout = html.Div(children=[ 
+    html.Div(
           children=[
               html.H1(
                   children="LinkedIn Vizualizer", className="header-title"
@@ -28,37 +26,40 @@ app.layout = html.Div(
           ],
                   className="header",
         ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                      figure=graphs.trend(connections_df)
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        figure=graphs.company_hist(connections_df)
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        figure=graphs.company_treemap(connections_df)
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        figure=graphs.position_treemap(connections_df)
-                    ),
-                    className="card",
-                ),
-            ],
-            className="wrapper",
-        ),
-    ]
-)
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            visualizer.parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
 
 if __name__ == "__main__":
   app.run_server(debug=True, use_reloader=True)
---
+++ app/visualizer.py
@@ -0,0 +1,66 @@
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import datetime
+import graphs
+
+import base64
+import datetime
+import io
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            connections_df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+            connections_df["Connected On"] = connections_df["Connected On"].apply(lambda x: datetime.datetime.strptime(x, "%d-%b-%y").strftime("%Y-%m-%d"))
+        else:
+           return html.Div([
+            'This file extension is not supported'
+        ])
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                      figure=graphs.trend(connections_df)
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        figure=graphs.company_hist(connections_df)
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        figure=graphs.company_treemap(connections_df)
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        figure=graphs.position_treemap(connections_df)
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
